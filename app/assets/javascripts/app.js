af1e1f28918030d225ee267cf493e573bf2e00b5
Khaya R. Dillon <yahyal0v3@yahoo.com>
2018-12-22 19:08:09 -0500

    Create mouseLeaveAction function in app js, remove onmouseleave event handler in books index erb and add onclick

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,26 @@
-// const bookInfo = document.getElementById("book-info")
+//const bookCoverDivs = document.querySelectorAll(".book-cover")
 
 function presentBookInfo(book) {
 	bookInfo = book.nextElementSibling
 	bookInfo.classList.remove("hidden")
+	mouseLeaveAction(book, "hover")
 }
 
 function hideBookInfo(book) {
 	bookInfo = book.nextElementSibling
 	bookInfo.classList.add("hidden")
+}
+
+function selectBook(book) {
+	presentBookInfo(book)
+	mouseLeaveAction(book, "selection")
+}
+
+function mouseLeaveAction(book, action) {
+	if (action === "hover") {
+		book.addEventListener("mouseleave", function() {
+			console.log("hit")
+			hideBookInfo(book)
+		})	
+	} 
 }
